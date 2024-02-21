35b9d6141ec87e44e7a4926999dc81f1cf746e1a
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2024-02-21 16:04:49 +0200

    feat(labor1): implemented f2

--- labor1/labor1/Views/MainPage.xaml.cs
+++ labor1/labor1/Views/MainPage.xaml.cs
@@ -9,18 +9,8 @@ namespace labor1.Views
         public MainPage()
         {
             InitializeComponent();
-            var version = typeof(MauiApp).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-                ?.InformationalVersion;
-            VersionLabel.Text = $".NET MAUI ver. {version?[..version.IndexOf('+')]}";
         }
 
-        private void entry_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            labelke.Text = "Hello user: " + e.NewTextValue;
-        }
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            DisplayAlert("title","Szia " + entry.Text,"cancel");
-        }
+        
     }
 }
