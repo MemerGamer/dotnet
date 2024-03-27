854af32180c87ed3d6e81bc45a6d3400c5fb7b20
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2024-03-27 14:37:52 +0200

    feat(labor6)

---
+++ Catalog Project/Catalog Project/Logic/CatalogController.cs
@@ -0,0 +1,25 @@
+﻿using Catalog_Project.Model;
+using Catalog_Project.Model.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog_Project.Logic
+{
+    public class CatalogController
+    {
+        private TMCatalogDB catalogDatabase;
+
+        public CatalogController()
+        {
+            this.catalogDatabase = new TMCatalogDB();
+        }
+
+        public List<Manufacturer> GetManufacturers()
+        {
+            return this.catalogDatabase.Manufacturer.ToList();
+        }
+    }
+}
---
+++ Catalog Project/Catalog Project/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Catalog_Project.Common.MVVM;
+using Catalog_Project.Logic;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog_Project.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public static CatalogController CatalogController;
+        public static MainWindowViewModel Instance;
+
+        public MainWindowViewModel()
+        {
+            CatalogController = new CatalogController();
+            MainWindowViewModel.Instance = this;
+            GetManufacturers();
+        }
+
+        private void GetManufacturers()
+        {
+            var temp = MainWindowViewModel.CatalogController.GetManufacturers();
+        }
+    }
+}
