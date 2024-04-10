a6df37f28c5836d59df8725b309a365e5448d754
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2024-04-10 13:05:36 +0300

    feat(labor7): implementation

---
+++ RutKiskacsa/RutKiskacsa/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace RutKiskacsa
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
